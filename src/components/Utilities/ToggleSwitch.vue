69e76f3ac2fd867962fe231f2855ba4ad65373f5
Santiago <santiagomorales333@yahoo.com>
2024-01-02 22:38:48 -0800

    switched slider to fading icon for toggleswitch theme toggler

--- src/components/Utilities/ToggleSwitch.vue
+++ src/components/Utilities/ToggleSwitch.vue
@@ -1,13 +1,20 @@
 <template>
-  <label class="switch">
-    <input type="checkbox" :checked="themeStore.isDark" @click="toggle" />
-    <span class="slider round"></span>
-  </label>
+  <Transition name="fade" mode="out-in">
+    <font-awesome-icon
+      icon="fa-solid fa-moon"
+      color="#ccc"
+      size="xl"
+      v-if="!themeStore.isDark"
+      @click="toggle" class="border2"
+    />
+    <font-awesome-icon icon="fa-solid fa-sun" color="#333" size="xl" v-else @click="toggle"  class="border"/>
+  </Transition>
 </template>
 
 <script setup>
 import { useThemeStore } from '@/stores/theme.js'
 import { useDark, useToggle } from '@vueuse/core'
+
 const themeStore = useThemeStore()
 const isDark = useDark({
   selector: 'body',
@@ -25,57 +32,15 @@ const toggle = () => {
 </script>
 
 <style lang="scss" scoped>
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-  border: 1px solid var(--slider-3);
 
-  // when in dark mode slider background
-  background-color: var(--slider-one);
+.border{
+  border: 2px solid var(--slider-3);
+  border-radius: 50px;
+  padding: 0.425rem;
 }
-.slider::before {
-  position: absolute;
-  content: '';
-  height: 23px;
-  width: 23px;
-  left: 4px;
-  top: 4px;
-  //the slider ball
-  background-color: var(--slider-3);
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
-input:checked + .slider {
-  //when in light mode slider background
-  background-color: var(--slider-one);
-}
-
-input:checked + .slider::before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-.slider.round {
-  border-radius: 34px;
-}
-.slider.slider.round::before {
-  border-radius: 50%;
+.border2{
+  border: 2px solid var(--slider-3);
+  border-radius: 50px;
+  padding: 0.425rem 0.625rem;
 }
 </style>
