3d02319c3013cb386ebb1e1f2e308b9d9df8da2a
Santiago <santiagomorales333@yahoo.com>
2024-01-04 16:02:20 -0800

    card transformation not allowed for screen size under 700

--- src/composables/cardTransformation.js
+++ src/composables/cardTransformation.js
@@ -1,17 +1,19 @@
-import {computed } from 'vue'
+import { computed } from 'vue'
 import { useMouseInElement } from '@vueuse/core'
+import { useWindowWidth } from '@/composables/windowWidth.js'
 
 export function useCardTransformation(target) {
-//   const target = ref(null)
+  //   const target = ref(null)
 
   const { elementX, elementY, isOutside, elementHeight, elementWidth } = useMouseInElement(target)
+  const { windowWidth } = useWindowWidth()
 
   const cardTransformation = computed(() => {
     const MAX_ROTATION = 15
     const rX = (MAX_ROTATION / 2 - (elementY.value / elementHeight.value) * MAX_ROTATION).toFixed(2)
     const rY = ((elementX.value / elementWidth.value) * MAX_ROTATION - MAX_ROTATION / 2).toFixed(2)
 
-    return isOutside.value
+    return isOutside.value || windowWidth.value < 700
       ? ''
       : `perspective(${elementWidth.value}px) rotateX(${rX}deg) rotateY(${rY}deg)`
   })
